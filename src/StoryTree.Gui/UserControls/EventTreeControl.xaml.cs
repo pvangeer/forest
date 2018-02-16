05fd3153cc7e6486ef84929aed9d1966e6d5f9c6
pvangeer <pieter.vangeer@deltares.nl>
2018-02-16 20:47:41 +0100

    Move presentation of storyboard to separate usercontrol

---
+++ src/StoryTree.Gui/UserControls/EventTreeControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StoryTree.Gui.UserControls
+{
+    /// <summary>
+    /// Interaction logic for EventTreeControl.xaml
+    /// </summary>
+    public partial class EventTreeControl : UserControl
+    {
+        public EventTreeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/StoryTree.Gui/UserControls/StoryBoardControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StoryTree.Gui.UserControls
+{
+    /// <summary>
+    /// Interaction logic for StoryBoardControl.xaml
+    /// </summary>
+    public partial class StoryBoardControl : UserControl
+    {
+        public StoryBoardControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
