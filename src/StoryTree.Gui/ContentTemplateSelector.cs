dd179aef97bd432f690020484453f900b763c36d
pvangeer <pieter.vangeer@deltares.nl>
2018-03-07 11:28:04 +0100

    Enable properties control selectoin

---
+++ src/StoryTree.Gui/ContentTemplateSelector.cs
@@ -0,0 +1,25 @@
+using System.Windows;
+using System.Windows.Controls;
+using StoryTree.Gui.ViewModels;
+
+namespace StoryTree.Gui
+{
+    public class ContentTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (container is ContentPresenter contentPresenter)
+            {
+                switch (item)
+                {
+                    case TreeEventViewModel _:
+                        return contentPresenter.Resources["TreeEventPropertiesTemplate"] as DataTemplate;
+                    case EventTreeViewModel _:
+                        return contentPresenter.Resources["EventTreePropertiesTemplate"] as DataTemplate;
+                }
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
