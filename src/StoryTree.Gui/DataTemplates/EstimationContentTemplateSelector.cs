4764b663133cd8839d1bef72196adbb6338baf9f
pvangeer <pieter.vangeer@deltares.nl>
2018-04-03 20:23:33 +0200

    Add last DataTemplate (empty)

--- src/StoryTree.Gui/DataTemplates/EstimationContentTemplateSelector.cs
+++ src/StoryTree.Gui/DataTemplates/EstimationContentTemplateSelector.cs
@@ -1,7 +1,7 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using StoryTree.Data.Estimations;
-using StoryTree.Data.Estimations.Classes;
 using StoryTree.Gui.ViewModels;
 
 namespace StoryTree.Gui.DataTemplates
@@ -10,24 +10,28 @@ namespace StoryTree.Gui.DataTemplates
     {
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            if (container is ContentPresenter contentPresenter)
+            if (!(container is ContentPresenter contentPresenter))
             {
-                if (!(contentPresenter.Content is ProbabilitySpecificationViewModelBase probabilitySpecification))
-                {
-                    // TODO: Return default template
-                    return null;
-                }
-
-                switch (probabilitySpecification.Type)
-                {
-                    case ProbabilitySpecificationType.Classes:
-                        return contentPresenter.Resources["ClassEstimationTemplate"] as DataTemplate;
-                    case ProbabilitySpecificationType.FixedValue:
-                        return contentPresenter.Resources["FixedValueEstimationTemplate"] as DataTemplate;
-                }
+                return base.SelectTemplate(item, container);
             }
 
-            return base.SelectTemplate(item, container);
+            if (!(contentPresenter.Content is ProbabilitySpecificationViewModelBase probabilitySpecification))
+            {
+                // TODO: Return default template
+                return null;
+            }
+
+            switch (probabilitySpecification.Type)
+            {
+                case ProbabilitySpecificationType.Classes:
+                    return contentPresenter.Resources["ClassEstimationTemplate"] as DataTemplate;
+                case ProbabilitySpecificationType.FixedValue:
+                    return contentPresenter.Resources["FixedValueEstimationTemplate"] as DataTemplate;
+                case ProbabilitySpecificationType.FixedFreqeuncy:
+                    throw new NotImplementedException();
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
