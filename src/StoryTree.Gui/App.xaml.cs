f2ccd54a00fb541853b960f0ac45f86547524f40
DIRECTORY\geer <Pieter.vanGeer@deltares.nl>
2018-04-25 16:54:36 +0200

    Try to handle all unhandled exceptions, excluding those thrown from a backgroundworker

--- src/StoryTree.Gui/App.xaml.cs
+++ src/StoryTree.Gui/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
+using StoryTree.Messaging;
 
 namespace StoryTree.Gui
 {
@@ -7,5 +9,16 @@ namespace StoryTree.Gui
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private readonly StoryTreeLog log = new StoryTreeLog(typeof(App));
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            log.Error($"Er is iets onverwachts misgegaan: {e.Exception.Message}");
+            e.Handled = true;
+        }
     }
 }
