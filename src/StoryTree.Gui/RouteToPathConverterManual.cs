7df4dac2db1b66d38ff899d9761e7da949a71697
DIRECTORY\geer <pieter.vangeer@deltares.nl>
2024-10-21 16:30:56 +0200

    Update risky packages and improve tree visualisation

--- src/StoryTree.Gui/RouteToPathConverterManual.cs
+++ src/StoryTree.Gui/RouteToPathConverterManual.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
 
@@ -10,17 +11,22 @@ namespace StoryTree.Gui
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values.Length != 9)
+            if (values.Length != 6)
                 return null;
+
+            var source = (Control)values[4];
+            var target = (Control)values[5];
 
             var sourceX = (double)values[0];
             var sourceY = (double)values[1];
-            var sourceWidth = (double)values[2];
-            var targetX = (double)values[4];
-            var targetY = (double)values[5];
-            var targetWidth = (double)values[6];
-            var startPoint = new Point(sourceX + 0.5 * sourceWidth, sourceY);
-            var endPoint = new Point(targetX - 0.5 * targetWidth, targetY);
+            var targetX = (double)values[2];
+            var targetY = (double)values[3];
+            
+            var startY = GetHeightFromTreeEventControl(sourceY,source);
+            var endY = GetHeightFromTreeEventControl(targetY, target);
+
+            var startPoint = new Point(sourceX + 0.5 * source.ActualWidth, startY);
+            var endPoint = new Point(targetX - 0.5 * target.ActualWidth, endY);
             var midPointX = (startPoint.X + endPoint.X) / 2.0;
 
             return new PathFigureCollection
@@ -33,6 +39,21 @@ namespace StoryTree.Gui
                         new LineSegment(endPoint, true)
                     }, false)
             };
+        }
+
+        private double GetHeightFromTreeEventControl(double controlY, Control control)
+        {
+            var contentPresenter = VisualTreeHelper.GetChild(control, 0);
+            var firstChild = (FrameworkElement)VisualTreeHelper.GetChild(contentPresenter, 0);
+            if (firstChild is Image)
+            {
+                return controlY;
+            }
+            var border = VisualTreeHelper.GetChild(firstChild, 0);
+            var innerGrid = VisualTreeHelper.GetChild(border, 0);
+            var textBlock = (FrameworkElement)VisualTreeHelper.GetChild(innerGrid, 2);
+
+            return controlY + control.ActualHeight / 2.0 - textBlock.ActualHeight - 1.5;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
